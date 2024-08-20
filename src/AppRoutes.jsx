de20c4dc76d48f6b5170e8fcfdc5ecce8a77a0cb
neerajsharma0392 <neeraj.sharma0392@gmail.com>
2024-08-20 16:01:42 +0000

    Added Protected Route

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { SignInPage } from "./authentication";
 import { NavBar } from "./navigation";
 import { ChatListPage } from "./chats/ChatListPage";
+import { ProtectRoute } from "./authentication/ProtectRoute";
 
-export const AppRoutes = () => {
+export const AppRoutes = ({user}) => {
   return (
     <Router>
       <NavBar></NavBar>
       <Routes>
+        <Route element={<ProtectRoute user={user} />}>
         <Route path="/" element={<ChatListPage/>}/>
+        </Route>
         <Route path="/sign-in" element={<SignInPage />} />
       </Routes>
     </Router>
