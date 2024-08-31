bf7dac7a21c5390da54541ca9e0adf703f916777
neerajsharma0392 <neeraj.sharma0392@gmail.com>
2024-08-31 16:14:52 +0000

    Added custom hook for get api calls added New Chat Creation page Added post api function

--- src/chats/ChatListPage.jsx
+++ src/chats/ChatListPage.jsx
@@ -1,4 +1,14 @@
+import { Link } from "react-router-dom";
+
 export const ChatListPage = () => {
-    return <h1>ChatListPage</h1>;
-  };
-  
+  return (
+    <div className="container">
+      <h1>Chats List</h1>
+      <hr />
+      <h1>Chats will be shown here - TBD</h1>
+      <Link to="/new-chat">
+        <button className="btn btn-primary">New Chat</button>
+      </Link>
+    </div>
+  );
+}
---
+++ src/chats/NewChatPage.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import {useGetCall,postApi} from '../services'
+import { useUserDetails } from "./../authentication";
+import { useNavigate } from "react-router-dom";
+
+export const NewChatPage = () => {
+  const [name, setName] = useState("");
+  const { user: currentUser } = useUserDetails();
+  const { isLoading, data: users } = useGetCall("/users", []);
+  const [members, setMembers] = useState([]);
+  const navigate = useNavigate();
+
+  const add = (id) => {
+    setMembers([...members, id]);
+  };
+
+  const createGroup = async (currentUserId) => {
+    const res = await postApi("/new-chat", {
+      name,
+      memberIds: [...members, currentUserId],
+    });
+
+    const newChatId = await res.json();
+    navigate(`/chat/${newChatId}`);
+  };
+
+  return (
+    <div className="container">
+      <h1>New Chat</h1>
+      <hr />
+      <input
+        className="form-control"
+        type="text"
+        placeholder="Enter a name for the group/chat"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <h3 className="mt-3">Add users</h3>
+      <ul className="list-group mt-3">
+        {users.map((user) => (
+          <li className="list-group-item" key={user.id}>
+            <label>{user.name}</label>
+            {members.includes(user.id) || user.id == currentUser.uid ? (
+              <button
+                onClick={() => add(user.id)}
+                style={{
+                  float: "right",
+                }}
+                className="btn btn-primary"
+                disabled
+              >
+                Added!
+              </button>
+            ) : (
+              <button
+                onClick={() => add(user.id)}
+                style={{
+                  float: "right",
+                }}
+                className="btn btn-primary"
+              >
+                Add
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+      <button
+        onClick={() => createGroup(currentUser.uid)}
+        className="btn btn-primary mt-3"
+      >
+        Create Group
+      </button>
+    </div>
+  );
+};
