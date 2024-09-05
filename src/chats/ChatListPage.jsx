638a52e8211a48d9d8661973b060cf270afdaa0c
neerajsharma0392 <49804856+neerajsharma0392@users.noreply.github.com>
2024-09-05 21:57:03 +0530

    added chat list (#7)

--- src/chats/ChatListPage.jsx
+++ src/chats/ChatListPage.jsx
@@ -1,11 +1,30 @@
 import { Link } from "react-router-dom";
+import {useUserDetails} from "../authentication";
+import {useGetCall} from "../services";
+import { Loader } from "../Loader";
+
 
 export const ChatListPage = () => {
+  const {user} = useUserDetails();
+  const {isLoading,data:chats}=useGetCall(`/users/${user.uid}/chats`);
+
+  if(isLoading){
+    return <Loader/>
+  }
+  console.log("chats",chats);
+
   return (
     <div className="container">
       <h1>Chats List</h1>
       <hr />
-      <h1>Chats will be shown here - TBD</h1>
+       {chats.map(chat=>(
+        <Link to={`/chat/${chat._id}`}>
+          <div className="list-item">
+            <h3>{chat.name}</h3>
+            <p>{chat.ids.length} Members</p>
+          </div>
+        </Link>
+       ))}
       <Link to="/new-chat">
         <button className="btn btn-primary">New Chat</button>
       </Link>
